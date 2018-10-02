f1a52ac5fa2ce5c26267b522407a61b1f7458962
Alona Varshal <thirdwave72@gmail.com>
2018-10-01 22:35:08 -0500

    Render templates using handlebars with data

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const express = require("express");
+const hbs = require("hbs");
 
 const app = express();
 
+app.set("view engine", "hbs");
 app.use(express.static(__dirname + "/public"));
 
 app.get("/", (req, res) => {
@@ -12,7 +14,10 @@ app.get("/", (req, res) => {
 });
 
 app.get("/about", (req, res) => {
-  res.send("About Page");
+  res.render("about.hbs", {
+    pageTitle: "About Page",
+    currentYear: new Date().getFullYear()
+  });
 });
 
 app.get("/bad", (req, res) => {
