9deaeb8687bfdff401fad20c9b127011d63b6f51
Alona Varshal <thirdwave72@gmail.com>
2018-10-01 22:12:14 -0500

    initial commit

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require("express");
+
+const app = express();
+
+app.get("/", (req, res) => {
+  // res.send("<h1>Hello Express!</h1>");
+  res.send({
+    name: "Alona"
+  });
+});
+
+app.get("/about", (req, res) => {
+  res.send("About Page");
+});
+
+app.get("/bad", (req, res) => {
+  res.send({
+    errorMessage: "Unable to fulfill this request"
+  });
+});
+
+app.listen(3000, () => {
+  console.log("listening at PORT 3000");
+});
